0fa4ad116042fdafbda3306db167c1dbb7e922be
刘骐源-jason <liuqiyuan_jason@outlook.com>
2024-04-27 15:03:56 +0800

    Create p1217 Create p7960 Finish p1217

---
+++ p1217/p1217.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+int l, r;
+bool check1(int x)
+{
+	if((1000 <= x && x <= 9999) || (100000 <= x && x <= 999999)) {
+        return 0;
+    }
+	return 1;
+} 
+bool check2(int x)
+{
+	int a[20], flag = 1;
+	while (x > 0)
+	{
+		a[flag] = x % 10;
+		x /= 10;
+		flag++;
+	} 
+	for (int i = 1; i <= flag / 2; i++)
+		if(a[i] != a[flag-i]){
+      return 0;
+    }
+	return 1;
+} 
+bool check3(int x)
+{
+	if(x == 2){
+        return 1;
+    }
+	for(int i = 2; i <= sqrt(x); i++)
+		if(x % i == 0){
+            return 0;
+        }
+	return 1;
+}
+int main()
+{
+	cin>>l>>r;
+	if(l % 2 == 0) l++; 
+	r = min(9999999, r);
+	for(int i = l; i <= r; i = i + 2)
+	{
+		if(check1(i) == 0||check2(i)==0||check3(i)==0){
+            continue;
+        } 
+		cout<<i<<endl;
+	}	
+	return 0;
+}
